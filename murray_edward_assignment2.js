d6f72adb01d620a49c145ea116c3080cf16aa50e
Edward M murray Jr <ampedej@fullsail.edu>
2012-09-05 20:19:01 -0400

    Added Procedure code to assignment

--- murray_edward_assignment2.js
+++ murray_edward_assignment2.js
@@ -1,1 +1,29 @@
-alert("JavaScript works!");
+//alert("JavaScript works!");
+
+// Edward M Murray Jr
+// SDI 1209
+// Assignment 2
+// Fantasy Football Assignment 2
+
+//Set variables
+var peopleGoing = 11;
+
+//Procedure
+function goingToParty(peopleGoing){
+	if (peopleGoing > 10) {
+		partyFun = "Looks like 10 people are going to the draft party. Do i have to pick people up on my way?" ;
+	}else{
+		partyFun = "Less than 10 are going, draft Party is going to be lame, I am not going";			
+	}
+
+}
+goingToParty(peopleGoing);
+console.log ("I am only going to the draft party if 10 people are going." + partyFun);
+
+//Boolean Function
+
+//Number Function
+
+//String Function
+
+//Array Function
